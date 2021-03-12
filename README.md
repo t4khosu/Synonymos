cc8eff0e76343cc669744a2e61afd9ebcd8e8458
t4khosu <t4khosu@gmail.com>
2021-03-12 16:53:01 +0100

    Refactor; Add output directory option

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Synonymos
 
 If you have ever wanted to change a perfectly normal text into some gibberish, this tool is going to help you!
-Pass a wiki article URL, string or some textfile as parameter and all non-stopwords will be switched with some random synonym.
-Since wiki articles can be quite long, only the summary will be translated.
+Pass a wiki article URL, string or some text file path as parameter and all non-stopwords will be switched with some random synonyms.
+Since wiki articles can be quite long, only the summary will be "translated".
 
 ## Setup
 * Python >= 3.8 
@@ -15,14 +15,20 @@ python syn.py -s <text>
 ```
 
 **Handle a .txt file** 
-```
+```shell script
 python syn.py -t <file path>
 ```
 
 **Handle a wikipedia entry** 
-```
+```shell script
 python syn.py -w <wiki title>
 ```
+
+**Set an output directory**
+```shell script
+python syn.py -s <some string> -o <output dir>
+```
+The output file will be called `.../syn.txt`
 
 ## An example of Wiki Texte
 
--- synonymos/core.py
+++ synonymos/core.py
@@ -1,6 +1,6 @@
 import argparse
+import os
 import random
-import sys
 
 import wikipedia
 from nltk.corpus import wordnet
@@ -12,9 +12,11 @@ def execute_from_command_line():
     parser.add_argument('-t', '--txt', dest='txt_path', help='.txt file path')
     parser.add_argument('-w', '--wiki', dest='wiki_url', help='Wikipedia url')
     parser.add_argument('-o', '--out', dest='out',
-                        help='If you want to store the result into some file, add the path here')
+                        help='If you want to store the result into some file, add the directory path here')
 
     args = parser.parse_args()
+
+    __check_output_dir(args.out)
 
     with open('stopwords.txt', 'r') as f_in:
         stopwords = f_in.read().split('\n')
@@ -22,7 +24,20 @@ def execute_from_command_line():
     text = __load_text(args)
     syn_text = __text_to_gibberish(text, stopwords)
 
-    print(syn_text)
+    __store_or_print(syn_text, args.out)
+
+
+def __check_output_dir(out_dir):
+    if out_dir and not os.path.isdir(out_dir):
+        raise OSError(f"{out_dir} does not exist...")
+
+
+def __store_or_print(result, out_dir):
+    if out_dir:
+        with open(os.path.join(out_dir, 'syn.txt'), 'w') as f_out:
+            f_out.write(result)
+    else:
+        print(result)
 
 
 def __load_text(args):
@@ -34,49 +49,56 @@ def __load_text(args):
     elif args.wiki_url:
         return wikipedia.summary(args.wiki_url)
     else:
-        print("No valid input")
-        sys.exit(0)
+        raise Exception("No valid input argument. Use `syn.py -h` for more information")
 
 
 def __text_to_gibberish(text, stopwords):
     gibberish = ''
 
     for line in text.split('\n'):
-        tokens = line.split()
-        syns = []
-        for token in tokens:
-            prefix = ''
-            suffix = ''
-
-            if not token[0].isalpha():
-                prefix = token[0]
-                token = token[1:]
-            if not token[-1].isalpha():
-                suffix = token[-1]
-                token = token[:-1]
-
-            if token in stopwords:
-                syns.append(prefix + token + suffix)
-                continue
-
-            if any(not char.isalpha() for char in token):
-                syns.append(prefix + token + suffix)
-                continue
-
-            token_syns = wordnet.synsets(token.lower())
-            if not token_syns:
-                syns.append(prefix + token + suffix)
-                continue
-
-            syn = random.choice(token_syns)
-            syn = syn.lemmas()[0].name()
-
-            if token[0].isupper():
-                syn = syn.capitalize()
-
-            syn = syn.replace('_', ' ')
-            syn = prefix + syn + suffix
-            syns.append(syn)
-        gibberish += ' '.join(syns) + '\n'
+        line_gibberish = __line_to_gibberish(line, stopwords)
+        gibberish += line_gibberish + '\n'
 
     return gibberish
+
+
+def __strip_token(token):
+    prefix = ''
+    suffix = ''
+
+    if not token[0].isalpha():
+        prefix = token[0]
+        token = token[1:]
+    if not token[-1].isalpha():
+        suffix = token[-1]
+        token = token[:-1]
+
+    return prefix, token, suffix
+
+
+def __line_to_gibberish(line, stopwords):
+    syns = []
+
+    for token in line.split():
+        prefix, sub_token, suffix = __strip_token(token)
+
+        if sub_token.lower() in stopwords or any(not char.isalpha() for char in sub_token):
+            syns.append(token)
+            continue
+
+        token_syns = list(set(syn.lemmas()[0].name() for syn in wordnet.synsets(sub_token.lower())))
+
+        if not token_syns:
+            syns.append(token)
+            continue
+
+        syn = random.choice(token_syns)
+
+        if sub_token[0].isupper():
+            syn = syn.capitalize()
+
+        syn = syn.replace('_', ' ')
+        syn = prefix + syn + suffix
+        syns.append(syn)
+
+    return ' '.join(syns)
